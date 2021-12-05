d558f1fad569602c8febf8650ed8922ac0404c90
Dandyk2605 <81756618+Dandyk2605@users.noreply.github.com>
2021-12-05 18:56:55 +0700

    Add files via upload

---
+++ 1918129tugas7/app/src/main/java/com/example/a1918129tugas7/MainActivity.java
@@ -0,0 +1,16 @@
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void create(View view){
+        Intent a = new
+                Intent(MainActivity.this,MainCreate.class);
+        startActivity(a);
+    }
+    public void read(View view){
+        Intent b = new Intent(MainActivity.this,MainRead.class);
+        startActivity(b);
+    }
+}
---
+++ 1918129tugas7/app/src/main/java/com/example/a1918129tugas7/MyDatabase.java
@@ -0,0 +1,4 @@
+package com.example.a1918129tugas7;
+
+public class MyDatabase {
+}
